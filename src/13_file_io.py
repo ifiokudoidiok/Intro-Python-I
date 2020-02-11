260e1d224d9eb4f35eb5263db9b34c0d98f85ba2
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2020-02-11 17:13:54 +0100

    completed task 14

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -21,6 +21,10 @@ with open(abs_file_path) as f:
     read_data = f.read()
     print(read_data)
 
+# with open('src/foo.txt', 'r') as f:
+#     read_data = f.read()
+# print(read_data)
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
